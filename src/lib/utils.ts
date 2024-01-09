d89eb8c6c6a3f0c38a5e61d40420741e6554f885
Yuri Almeida <73500564+yalmeidarj@users.noreply.github.com>
2024-01-09 09:44:40 -0500

    new colour scheme for houseCard

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -6,29 +6,23 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 export function getConditionalClass(statusAttempt: string, consent: string) {
+  // Handle cases where "Door Knock Attempt" is part of the string
+  if (statusAttempt.includes("Door Knock Attempt")) {
+    return "bg-blue-200 text-blue-900";
+  }
+
   switch (statusAttempt) {
     case "No Attempt":
       return "bg-gray-300 text-gray-800";
-    case "Door Knock Attempt 1":
-      return "bg-blue-200 text-blue-900";
-    case "Door Knock Attempt 2":
+    case "Drop Type Unverified":
+    case "Drop Type Unverified Needs Verification":
       return "bg-yellow-200 text-yellow-900";
-    case "Door Knock Attempt 3":
-      return "bg-indigo-200 text-indigo-900";
-    case "Door Knock Attempt 4":
-      return "bg-orange-200 text-orange-900";
-    case "Door Knock Attempt 5":
-      return "bg-purple-200 text-purple-900";
-    case "Door Knock Attempt 6":
-      return "bg-red-200 text-red-900";
     case "Consent Final":
-      if (consent.toLowerCase() == "yes") {
-        return "bg-green-200 text-green-900";
-      } else if (consent.toLowerCase() == "no") {
-        return "bg-red-200 text-red-900";
-      } else {
-        return "bg-gray-200 text-gray-900";
-      }
+      return consent.toLowerCase() === "yes"
+        ? "bg-green-200 text-green-900"
+        : consent.toLowerCase() === "no"
+        ? "bg-red-200 text-red-900"
+        : "bg-gray-200 text-gray-900";
     case "Consent Final Yes":
       return "bg-green-200 text-green-900";
     case "Consent Final No":
@@ -42,8 +36,9 @@ export function getConditionalClass(statusAttempt: string, consent: string) {
   }
 }
 
+
 export const defaultValues = {
-  defaultPerPage: 10,
+  defaultPerPage: 20,
   defaultPage: 1,
 }
 export const SalesForce = {
