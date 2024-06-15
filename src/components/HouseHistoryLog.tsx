cd28525b625c3e028b82504b4ed2cdbb47cfe0bb
Yuri Almeida <73500564+yalmeidarj@users.noreply.github.com>
2024-06-15 17:49:43 -0400

    types ajust for compactTable

--- src/components/HouseHistoryLog.tsx
+++ src/components/HouseHistoryLog.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { format } from "date-fns";
-import { DateTime } from "luxon";
 
 interface LogEntry {
     name: string | null;
@@ -16,13 +15,16 @@ interface LogEntry {
     userName: string | null;
 }
 
-
 interface HouseHistoryLogProps {
     logs: LogEntry[];
 }
 
 const HouseHistoryLog: React.FC<HouseHistoryLogProps> = ({ logs }) => {
-    function isDataValid(data: string | Date) {
+
+    function isDataValid(data: string | Date | null): boolean {
+        if (data === null) {
+            return false;
+        }
         if (typeof data === "string") {
             const parts = data.split("->").map(part => part.trim());
             if (parts.length === 2 && (parts[1] === "null" || parts[0] === parts[1])) {
@@ -32,27 +34,28 @@ const HouseHistoryLog: React.FC<HouseHistoryLogProps> = ({ logs }) => {
         return true;
     }
 
-    function formatData(data: string | Date): string {
-        if (data instanceof Date) {
-            const newDate = format(new Date(data), 'MMM do')
-            console.log(`FORMATED DATE: --->${newDate.toLocaleString()}`)
-            return newDate.toLocaleString();
+    function formatData(data: string | Date | null): string {
+        if (data === null) {
+            return "";
         }
-        return data;
+        if (data instanceof Date) {
+            const newDate = format(new Date(data), 'MMM do');
+            return newDate.toString();
+        }
+        return data as string;
     }
 
     return (
         <div className="space-y-2">
             {logs.map((log, index) => (
                 <div key={index} className="p-2 bg-gray-100 flex flex-col rounded-md shadow-sm">
-                    {/* <div><strong>By:</strong>{log.userName}</div> */}
-                    <div  className="p-2 bg-gray-100 flex">
-                    {(Object.keys(log) as (keyof LogEntry)[]).map((key) =>
-                        isDataValid(log[key]) && (
-                            <div key={key}><strong>{key}:</strong> {formatData(log[key])}</div>
-                        )
+                    <div className="p-2 bg-gray-100 flex">
+                        {(Object.keys(log) as (keyof LogEntry)[]).map((key) =>
+                            isDataValid(log[key]) && (
+                                <div key={key}><strong>{key}:</strong> {formatData(log[key])}</div>
+                            )
                         )}
-                        </div>
+                    </div>
                 </div>
             ))}
         </div>
