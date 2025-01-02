ee6ba2602f842df26a230906b88cd1b710c19d5d
Yuri Almeida <73500564+yalmeidarj@users.noreply.github.com>
2025-01-01 23:37:27 -0500

    not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -4,7 +4,7 @@ export default function NotFound() {
     return (
         <div>
             <h2>Not Found</h2>
-            <p>Could not find requested resource</p>
+            <p>The page you are looking for does not exist</p>
             <Link href="/">Go Home</Link>
         </div>
     )
